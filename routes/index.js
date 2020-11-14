c8c4065807a7efd41f23b381a3b913407738fc18
Roy-Arne Brussel <29131401+Roybrussel@users.noreply.github.com>
2020-11-14 14:19:23 +0100

    First commit

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+/* GET home page */
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+/* GET profile page */
+router.get("/profile", (req, res, next) => {
+  res.render("profile");
+});
+
+module.exports = router;
