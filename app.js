95c7d71b8cb44354502aa36535cf6214a09d31f2
Roy-Arne Brussel <29131401+Roybrussel@users.noreply.github.com>
2020-11-16 20:46:41 +0100

    Added styling to user-profile page. Working on the alignment of the travel-boards.

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express = require('express');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
-const path = require('path');
-const passport = require('passport');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const favicon = require("serve-favicon");
+const logger = require("morgan");
+const path = require("path");
+const passport = require("passport");
+
+hbs.registerPartials("partials_absolute_path");
 
 //DB config
-require('./configs/db-config');
+require("./configs/db-config");
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(
-  `${app_name}:${path.basename(__filename).split('.')[0]}`
+const app_name = require("./package.json").name;
+const debug = require("debug")(
+  `${app_name}:${path.basename(__filename).split(".")[0]}`
 );
 
 const app = express();
 
-require('./configs/session.config')(app);
-require('./configs/passport.config');
+require("./configs/session.config")(app);
+require("./configs/passport.config");
 
 // Middleware Setup
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -32,29 +34,30 @@ app.use(passport.session());
 
 // Express View engine setup
 app.use(
-  require('node-sass-middleware')({
-    src: path.join(__dirname, 'public'),
-    dest: path.join(__dirname, 'public'),
+  require("node-sass-middleware")({
+    src: path.join(__dirname, "public"),
+    dest: path.join(__dirname, "public"),
     sourceMap: true,
   })
 );
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
 // default value for title local
-app.locals.title = 'Travel Board';
+app.locals.title = "Travel Board";
 
 //Routes middleware
-const index = require('./routes/index');
-app.use('/', index);
+const index = require("./routes/index");
+app.use("/", index);
 
-const user = require('./routes/user.routes');
-app.use('/', user);
+const user = require("./routes/user.routes");
+app.use("/", user);
 
-const auth = require('./routes/auth.routes');
-app.use('/', auth);
+const auth = require("./routes/auth.routes");
+app.use("/", auth);
 
 module.exports = app;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,32 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const Travelboard = require('../models/Travelboard.model');
-const User = require('../models/User.model');
+const Travelboard = require("../models/Travelboard.model");
+const User = require("../models/User.model");
 
-router.get('/profile', (req, res, next) => {
+router.get("/profile", (req, res, next) => {
   User.findById(req.session.currentUser._id)
-    .populate('travelBoards')
+    .populate("travelBoards")
     .then((foundUser) => {
-      res.render('user/user-profile', {
+      res.render("user/user-profile", {
         userInSession: foundUser || null,
       });
     })
     .catch((err) => `Error while getting user from the DB: ${err}`);
 });
 
-router.get('/edit-profile/:id', (req, res, next) => {
+router.get("/edit-profile/:id", (req, res, next) => {
   if (!req.session.currentUser) {
-    res.redirect('/login');
+    res.redirect("/login");
     return;
   }
 
-  res.render('user/edit-user-profile', {
+  res.render("user/edit-user-profile", {
     userInSession: req.session.currentUser,
   });
 });
 
-router.post('/edit-profile/:id', (req, res, next) => {
+router.post("/edit-profile/:id", (req, res, next) => {
   const { id } = req.params;
 
   const {
@@ -49,23 +49,23 @@ router.post('/edit-profile/:id', (req, res, next) => {
     { new: true }
   )
     .then(() => {
-      res.redirect('/profile');
+      res.redirect("/profile");
     })
     .catch((error) => next(error));
 });
 
-router.get('/add-travel-board/:userid', (req, res, next) => {
+router.get("/add-travel-board/:userid", (req, res, next) => {
   if (!req.session.currentUser) {
-    res.redirect('/login');
+    res.redirect("/login");
     return;
   }
 
-  res.render('add-travel-board', {
+  res.render("add-travel-board", {
     userInSession: req.session.currentUser,
   });
 });
 
-router.post('/add-travel-board/:userid', (req, res, next) => {
+router.post("/add-travel-board/:userid", (req, res, next) => {
   const { userid } = req.params;
   const { country, experienceInput, travelBoardPictureUrl } = req.body;
 
@@ -78,7 +78,7 @@ router.post('/add-travel-board/:userid', (req, res, next) => {
     .then((newBoard) =>
       User.findByIdAndUpdate(userid, { $push: { travelBoards: newBoard._id } })
     )
-    .then(() => res.redirect('/profile'))
+    .then(() => res.redirect("/profile"))
     .catch((error) => `Error while creating a new Travel Board: ${error}`);
 });
 
