6f4d45a4b736add824c3d935fb52923df85c7af4
Roy-Arne Brussel <29131401+Roybrussel@users.noreply.github.com>
2020-11-17 16:51:25 +0100

    Changed the input for the about field in order for it to show on the edit-user-profile page.

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,15 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const fileUploader = require('../configs/cloudinary.config');
-const Travelboard = require('../models/Travelboard.model');
-const User = require('../models/User.model');
+const fileUploader = require("../configs/cloudinary.config");
+const Travelboard = require("../models/Travelboard.model");
+const User = require("../models/User.model");
 
-router.get('/profile', (req, res, next) => {
+router.get("/profile", (req, res, next) => {
   if (req.session.currentUser) {
     User.findById(req.session.currentUser._id)
-      .populate('travelBoards')
+      .populate("travelBoards")
       .then((foundUser) => {
-        res.render('user/user-profile', {
+        res.render("user/user-profile", {
           userInSession: foundUser || null,
         });
       })
@@ -17,24 +17,24 @@ router.get('/profile', (req, res, next) => {
         `Error while getting user from the DB: ${err}`;
       });
   } else {
-    res.redirect('login');
+    res.redirect("login");
   }
 });
 
-router.get('/edit-profile/:id', (req, res, next) => {
+router.get("/edit-profile/:id", (req, res, next) => {
   if (!req.session.currentUser) {
-    res.redirect('/login');
+    res.redirect("/login");
     return;
   }
 
-  res.render('user/edit-user-profile', {
+  res.render("user/edit-user-profile", {
     userInSession: req.session.currentUser,
   });
 });
 
 router.post(
-  '/edit-profile/:id',
-  fileUploader.single('profilePictureUrl'),
+  "/edit-profile/:id",
+  fileUploader.single("profilePictureUrl"),
   (req, res, next) => {
     const { id } = req.params;
 
@@ -67,24 +67,24 @@ router.post(
       { new: true }
     )
       .then(() => {
-        res.redirect('/profile');
+        res.redirect("/profile");
       })
       .catch((error) => next(error));
   }
 );
 
-router.get('/add-travel-board/:userid', (req, res, next) => {
+router.get("/add-travel-board/:userid", (req, res, next) => {
   if (!req.session.currentUser) {
-    res.redirect('/login');
+    res.redirect("/login");
     return;
   }
 
-  res.render('add-travel-board', {
+  res.render("add-travel-board", {
     userInSession: req.session.currentUser,
   });
 });
 
-router.post('/add-travel-board/:userid', (req, res, next) => {
+router.post("/add-travel-board/:userid", (req, res, next) => {
   const { userid } = req.params;
   const { country, experienceInput, travelBoardPictureUrl } = req.body;
 
@@ -97,7 +97,7 @@ router.post('/add-travel-board/:userid', (req, res, next) => {
     .then((newBoard) =>
       User.findByIdAndUpdate(userid, { $push: { travelBoards: newBoard._id } })
     )
-    .then(() => res.redirect('/profile'))
+    .then(() => res.redirect("/profile"))
     .catch((error) => `Error while creating a new Travel Board: ${error}`);
 });
 
