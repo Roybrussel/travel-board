84ef4931336582ccfec77a4dab47db1dddcc28da
Nina van den Braak <ninavandenbraak@gmail.com>
2020-11-19 12:20:58 +0100

    heroku try

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const express = require("express");
-const favicon = require("serve-favicon");
-const logger = require("morgan");
-const path = require("path");
-const passport = require("passport");
-const hbs = require("hbs");
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const path = require('path');
+const passport = require('passport');
+const hbs = require('hbs');
 
-hbs.registerPartials(__dirname + "/views/partials");
+hbs.registerPartials(__dirname + '/views/partials');
 
 //DB config
-require("./configs/db-config");
+require('./configs/db-config');
 
-const app_name = require("./package.json").name;
-const debug = require("debug")(
-  `${app_name}:${path.basename(__filename).split(".")[0]}`
+const app_name = require('./package.json').name;
+const debug = require('debug')(
+  `${app_name}:${path.basename(__filename).split('.')[0]}`
 );
 
 const app = express();
 
-require("./configs/session.config")(app);
-require("./configs/passport.config");
+require('./configs/session.config')(app);
+require('./configs/passport.config');
 
 // Middleware Setup
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -35,40 +35,35 @@ app.use(passport.session());
 
 // Express View engine setup
 app.use(
-  require("node-sass-middleware")({
-    src: path.join(__dirname, "public"),
-    dest: path.join(__dirname, "public"),
+  require('node-sass-middleware')({
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
     sourceMap: true,
   })
 );
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "hbs");
-app.use(express.static(path.join(__dirname, "public")));
-app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 // default value for title local
-app.locals.title = "Travel Board";
+app.locals.title = 'Travel Board';
 
 //Routes middleware
-const index = require("./routes/index");
-app.use("/", index);
+const index = require('./routes/index');
+app.use('/', index);
 
-const user = require("./routes/user.routes");
-app.use("/", user);
+const user = require('./routes/user.routes');
+app.use('/', user);
 
-const auth = require("./routes/auth.routes");
-app.use("/", auth);
+const auth = require('./routes/auth.routes');
+app.use('/', auth);
 
-const board = require("./routes/board.routes");
-app.use("/", board);
+const board = require('./routes/board.routes');
+app.use('/', board);
 
-const city = require("./routes/city.routes");
-app.use("/", city);
-
-function countChars(obj) {
-  var strLength = obj.value.length;
-  document.getElementById("currentEditProfile").innerHTML = strLength;
-}
+const city = require('./routes/city.routes');
+app.use('/', city);
 
 module.exports = app;
