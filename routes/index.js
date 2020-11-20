fbf8b9bfaec2543320978073034c36d792989ea8
Roy-Arne Brussel <29131401+Roybrussel@users.noreply.github.com>
2020-11-20 14:19:53 +0100

    Changed header styling

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
   if (!req.session.currentUser) {
-    res.render('index');
+    res.render("index");
     return;
   }
 
-  res.render('index', {
+  res.render("index", {
     userInSession: req.session.currentUser,
   });
 });
 
+router.get("/test", (req, res, next) => res.render("test"));
+
 function countChars(obj) {
   var strLength = obj.value.length;
-  document.getElementById('currentEditProfile').innerHTML = strLength;
+  document.getElementById("currentEditProfile").innerHTML = strLength;
 }
 
 module.exports = router;
