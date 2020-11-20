2bfb5d488d18bb0c6699061cde5b53846407d59f
Nina van den Braak <ninavandenbraak@gmail.com>
2020-11-20 14:50:06 +0100

    default profile picture with new settings

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,16 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
 /* GET home page */
-router.get("/", (req, res, next) => {
+router.get('/', (req, res, next) => {
   if (!req.session.currentUser) {
-    res.render("index");
+    res.render('index');
     return;
   }
 
-  res.render("index", {
+  res.render('index', {
     userInSession: req.session.currentUser,
   });
 });
-
-router.get("/test", (req, res, next) => res.render("test"));
-
-function countChars(obj) {
-  var strLength = obj.value.length;
-  document.getElementById("currentEditProfile").innerHTML = strLength;
-}
 
 module.exports = router;
